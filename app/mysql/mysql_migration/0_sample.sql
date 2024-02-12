7102a4a4cc6a1663bcaf4d3b628bfe10b2b7a42b
JunMasaki59 <jy13.27.813@gmail.com>
2024-02-12 17:53:21 +0900

    migration適用

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,5 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+CREATE UNIQUE INDEX login ON user (mail, password);
+CREATE UNIQUE INDEX department_role ON department_role_member (user_id, department_id, role_id);
+CREATE UNIQUE INDEX department ON department(department_id);
+CREATE UNIQUE INDEX role ON role(role_id);

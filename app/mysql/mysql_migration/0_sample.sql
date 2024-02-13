b99f90e05b40d1451654573a95344f929be05b0e
k <k.fucunaga@gmail.com>
2024-02-13 10:57:57 +0900

    add index

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,6 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+CREATE UNIQUE INDEX user ON user (user_id, office_id);
+-- CREATE UNIQUE INDEX department_role_member ON department_role_member (department_id, role_id);
+CREATE UNIQUE INDEX skill ON skill(skill_id);
+CREATE UNIQUE INDEX skill_member ON skill_member(skill_id);
+CREATE UNIQUE INDEX file ON file(file_id);
